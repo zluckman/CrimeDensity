fdf48a1f80d2fada69b52088134472bd21d60b94
Jason Green <jgreen9797@gmail.com>
2018-04-25 15:48:34 -0500

    Revert "Update crimespercapita view"

--- TestTable.sql
+++ TestTable.sql
@@ -1,6 +1,6 @@
 #CREATE table testTable (id VARCHAR(4), name VARCHAR(20));
 #drop table testtable;
-drop table population;
+
 create table if not exists population(
 	fips_code INT(10),
     geography VARCHAR(40),
@@ -16,7 +16,6 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-drop table crimedata;
 create table if not exists crimedata(
 	county VARCHAR(40),
     agency VARCHAR(40),
@@ -42,7 +41,7 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-drop table policedata;
+
 create table if not exists policedata(
 	county VARCHAR(40),
     pd VARCHAR(40),
@@ -69,62 +68,6 @@ SELECT * FROM population;
 SELECT * FROM crimedata;
 SELECT * FROM policedata;
 
-drop view policeandcrime;
-
-create view policeandcrime as
-	select c.county, count(c.agency) as agencies, c.year, sum(c.index_total)/sum(p.grand_total) as crimespercop, sum(c.index_total) as crimetotal, sum(c.violent_total) as violenttotal, sum(c.murder) as murder, sum(c.rape) as rape, sum(c.robbery) as robbery, sum(c.aggravated_assault) as aggravatedassault, sum(c.property_total) as propertytotal, sum(c.burglary) as burglary, sum(c.larceny) as larceny, sum(c.motor_vehicle_theft) as vehicletheft, count(p.pd) as pdcount, sum(p.sworn_full_time) as swornfulltime, sum(p.grand_total) as policegrandtotal
-    from crimedata c, policedata p
-    where c.county = p.county and c.year = p.year and c.agency = p.pd
-    group by c.county, c.year
-    order by c.year, c.county;
-
-select * from policeandcrime;
+drop table population, crimedata, policedata;
 
 
-
-
-
-
-
-
-
-
-
-drop view crimepercapita;
-
-create view crimepercapita as
-	select c.county, count(c.agency) as agencies, c.year, pop.population, sum(c.index_total)/pop.population as crimesperperson, sum(c.index_total), sum(c.violent_total)/pop.population as violentperperson, sum(c.violent_total), sum(c.murder)/pop.population as murderperperson, sum(c.murder), sum(c.rape)/pop.population as rapeperperson, sum(c.rape), sum(c.robbery)/pop.population as robberyperperson, sum(c.robbery), sum(c.aggravated_assault)/pop.population as aggravatedassaultperperson, sum(c.aggravated_assault), sum(c.property_total)/pop.population as propertyperperson, sum(c.property_total), sum(c.burglary)/pop.population as burglaryperperson, sum(c.burglary), sum(c.larceny)/pop.population as larcenyperperson, sum(c.larceny), sum(c.motor_vehicle_theft)/pop.population as vehicletheftperperson, sum(c.motor_vehicle_theft)
-    from crimedata c, population pop
-    where c.county = pop.county and c.year = pop.year
-    group by c.county, c.year
-    order by c.year, c.county;
-
-select * from crimepercapita;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- drop view policeandcrime;
--- 
--- create view policeandcrime as
--- 	select c.county, count(c.agency) as agencies, c.year, sum(c.index_total)/sum(p.grand_total) as crimespercop, sum(c.index_total) as crimetotal, sum(c.violent_total) as violenttotal, sum(c.murder) as murder, sum(c.rape) as rape, sum(c.robbery) as robbery, sum(c.aggravated_assault) as aggravatedassault, sum(c.property_total) as propertytotal, sum(c.burglary) as burglary, sum(c.larceny) as larceny, sum(c.motor_vehicle_theft) as vehicletheft, count(p.pd) as pdcount, sum(p.sworn_full_time) as swornfulltime, sum(p.grand_total) as policegrandtotal
---     from crimedata c, policedata p
---     where c.county = p.county and c.year = p.year and c.agency = p.pd
---     group by c.county, c.year
---     order by c.year, c.county;
--- 
--- select * from policeandcrime;
---     

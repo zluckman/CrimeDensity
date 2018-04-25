ba2063b28f9eec6d790faf849d0eeb301a382698
jgreen9797 <jgreen9797@gmail.com>
2018-04-24 20:55:05 -0500

    Add crimedata Table

--- TestTable.sql
+++ TestTable.sql
@@ -16,3 +16,32 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
+create table if not exists crimedata(
+	county VARCHAR(40),
+    agency VARCHAR(40),
+    year INT(4),
+    months_reported INT(4),
+    index_total INT(10),
+    violent_total INT(10),
+    murder INT(10),
+    rape INT(10),
+    robbery INT(10),
+    aggravated_assault INT(10),
+    property_total INT(10),
+    burglary INT(10),
+    larceny INT(10),
+    motor_vehicle_theft INT(10),
+    region VARCHAR(40)
+);
+
+LOAD DATA LOCAL INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/Index_Crimes_by_County_and_Agency__Beginning_1990.csv' 
+INTO TABLE crimedata 
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+SELECT * FROM crimedata;
+
+-- drop table population;
+-- drop table crimedata;
